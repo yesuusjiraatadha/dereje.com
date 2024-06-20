7629b5aa2e12430dbc9a9bd10921889f28dde53b
dereje <152821376+yesuusjiraatadha@users.noreply.github.com>
2024-06-21 00:16:05 +0300

    info.js

--- info.js
+++ info.js
@@ -13,3 +13,21 @@ let typed = new Typed('.auto-input', {
     backDelay: 2000,
     loop: true
 });
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('nav ul li a');
+window.addEventListener('scroll' ,function (){
+    const scrollpos =window.scrollY+20
+    sections.forEach(section =>{
+        if(scrollpos>section.offsetTop && scrollpos (section.offsetTop+section.offsetHeight)){
+            navlinks.forEach(link=>{
+               link.classList.remove('active');
+                MenuBtn.classList.remove('active');
+               if(section.getAttribute('id')===link.getAttribute('href').substring(1)){
+                link.classList.add('active');
+               }
+            }) ; 
+        }
+    });
+
+});
